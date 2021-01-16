778977b7c57a64e3ac08f4cfa53b0851e1547f2c
BuildTools <unconfigured@null.spigotmc.org>
2021-01-16 05:13:39 +0000

    rewrote send commands for new version of discord.py

--- bernke.py
+++ bernke.py
@@ -26,80 +26,82 @@ def on_message(message):
     # Rat react any posts in the rat zone channel
     if str(message.channel) == "rat-zone-memorial-breakfast":
         yield from client.add_reaction(message, "\U0001F400")
-   
+
     # Prevent infinite loop
     if message.author == client.user:
         return   
+    channel = message.channel
 
     # Awful if statement for bot commands
     if message.content.startswith('!everyone'):
         msg = '@everyone lmao'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!takerate'):
         rate = 'Q' + str(r.randint(1,4)) + ' Take'
         msg = rate.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!takematrix'):
         msg = 'Q1 = Hot and Right, Q2 = Hot and Wrong, Q3 = Cold and Wrong, Q4 = Cold and Right'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!thisi'):
         msg = 'This, but ironically'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!thisu'):
         msg = 'This, but unironically'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!child'):
         msg = 'Did a child write this?'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!model'):
         msg = "What's your model?".format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!failure'):
         msg = '{0.author.mention} is a market failure'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!balls'):
         msg = 'Do it no balls'.format(message)
-        yield from client.send_message(message.channel, msg)
-    elif 'bitcoin' in message.content:
+        yield from channel.send(msg)
+    elif 'bitcoin' in message.content.lower():
         msg = '>bitcoin. lmao nice meme'.format(message)
-        yield from client.send_message(message.channel, msg, tts=True)
-    elif 'stupid' in message.content:
+        yield from channel.send(msg)
+    elif 'stupid' in message.content.lower():
         msg = discord.Embed()
         msg.set_image(url="https://i.imgur.com/LFIi7yS.jpg")
-        yield from client.send_message(message.channel, embed=msg)
+        yield from channel.send(embed=msg)
     elif message.content.startswith('!god'):
         msg = discord.Embed()
         msg.set_image(url="https://i.imgur.com/pZi485H.jpg")
-        yield from client.send_message(message.channel, embed=msg)
+        yield from channel.send(embed=msg)
     elif message.content.startswith('!GOD'):
         msg = discord.Embed()
         msg.set_image(url="https://i.imgur.com/VKqSxBH.jpg")
-        yield from client.send_message(message.channel, embed=msg)
+        yield from channel.send(embed=msg)
     elif message.content.startswith('!dumbdumb'):
         msg = 'lol ur big dumb'.format(message)
-        yield from client.send_message(message.channel, embed=msg)
+        yield from channel.send(embed=msg)
     elif message.content.startswith('!specifictakerate'):
         msg = '(' + str(r.randint(-999999999,999999999)) + ', ' + str(r.randint(-999999999,999999999)) + ') Use !takerate for a more general take'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!census'):
         msg = 'https://www2.census.gov/library/publications/decennial/1860/population/1860a-31.pdf'
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!iconic'):
         msg = discord.Embed()
         msg.set_image(url="https://i.imgur.com/k7FjY1m.jpg")
-        yield from client.send_message(message.channel, embed=msg)
+        yield from channel.send(embed=msg)
     elif message.content.startswith('!git'):
         msg = 'git commit -m "no need for pull request already reviewed changes dw"\ngit push origin master'
-        yield from client.send_message(message.channel, msg)
-    elif 'ethereum' in message.content:
+        yield from channel.send(msg)
+    elif 'ethereum' in message.content.lower():
         msg = '>ethereum. lmao nice meme'.format(message)
-        yield from client.send_message(message.channel, msg, tts=True)
+        yield from channel.send(msg)
     elif message.content.startswith('!value'):
         msg = 'https://www.youtube.com/watch?v=WnPjqt6vEzA'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
     elif message.content.startswith('!trailer'):
         msg = 'https://youtu.be/z7-LxU4SqCw'.format(message)
-        yield from client.send_message(message.channel, msg)
+        yield from channel.send(msg)
+
 
 @client.event
 @asyncio.coroutine
